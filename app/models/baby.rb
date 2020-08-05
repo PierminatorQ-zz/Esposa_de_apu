1eebff82d7eea200a8945aa7615a0f6513a8405f
PierminatorQ <pierrequintero@gmail.com>
2020-08-05 19:58:13 -0400

    show methods added

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -19,4 +19,36 @@ class Baby < ApplicationRecord
     def to_s
         name
     end
+
+    #methods for show.html.erb table data
+
+        #method for calculate how many times has breadfeeding each baby
+        def breast_feeding_times_today
+            breast_feedings.where("created_at >= :start_date AND created_at <= :end_date", {start_date: Time.now.beginning_of_day, end_date: Time.now}).count
+        end
+
+        #method for calculate last breastfeeding each baby
+        def last_time_today
+            
+            #show a empty data if doesn exist any last record
+            if breast_feedings > 0
+            breast_feedings.last.created_at
+            else
+                ""
+            end
+            
+        end
+
+        #method for calculate duration for all breastfeedings today
+        def breast_feeding_duration_today
+            breast_feedings.where("created_at >= :start_date AND created_at <= :end_date", {start_date: Time.now.beginning_of_day, end_date: Time.now}).sum(:duration)
+        end
+
+        #method for calculate quantity for all breastfeedings today
+        def breast_feeding_quantity_today
+            breast_feedings.where("created_at >= :start_date AND created_at <= :end_date", {start_date: Time.now.beginning_of_day, end_date: Time.now}).sum(:quantity)
+        end
+
+
+
 end
